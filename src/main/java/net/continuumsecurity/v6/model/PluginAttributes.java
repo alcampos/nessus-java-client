3b1147ff4253acf2f131ce495dbb3506fa072ed3
alcampos <alexander.campos@mercadolibre.com>
2015-10-19 17:05:10 -0300

    Rename attribute

--- src/main/java/net/continuumsecurity/v6/model/PluginAttributes.java
+++ src/main/java/net/continuumsecurity/v6/model/PluginAttributes.java
@@ -11,59 +11,75 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class PluginAttributes {
 	private String synopsis;
 	private String description;
-	private RiskInformation riskinformation;
+	private RiskInformation risk_information;
 	private String plugin_name;
 	private List<String> see_also;
 	private String fname;
 	private PluginInformation plugin_information;
 	private String solution;
+
 	public String getSynopsis() {
 		return synopsis;
 	}
+
 	public void setSynopsis(String synopsis) {
 		this.synopsis = synopsis;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public RiskInformation getRiskinformation() {
-		return riskinformation;
+
+	public RiskInformation getRisk_information() {
+		return risk_information;
 	}
-	public void setRiskinformation(RiskInformation riskinformation) {
-		this.riskinformation = riskinformation;
+
+	public void setRisk_information(RiskInformation riskinformation) {
+		this.risk_information = riskinformation;
 	}
+
 	public String getPlugin_name() {
 		return plugin_name;
 	}
+
 	public void setPlugin_name(String plugin_name) {
 		this.plugin_name = plugin_name;
 	}
+
 	public List<String> getSee_also() {
 		return see_also;
 	}
+
 	public void setSee_also(List<String> see_also) {
 		this.see_also = see_also;
 	}
+
 	public String getFname() {
 		return fname;
 	}
+
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
+
 	public PluginInformation getPlugin_information() {
 		return plugin_information;
 	}
+
 	public void setPlugin_information(PluginInformation plugin_information) {
 		this.plugin_information = plugin_information;
 	}
+
 	public String getSolution() {
 		return solution;
 	}
+
 	public void setSolution(String solution) {
 		this.solution = solution;
 	}
-	
+
 }
