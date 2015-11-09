3343a3fa574b8dcccb4ff3b1e192c2eaab67c182
alcampos <alexander.campos@mercadolibre.com>
2015-11-09 16:42:07 -0300

    Added Output

---
+++ src/main/java/net/continuumsecurity/v6/model/Output.java
@@ -0,0 +1,59 @@
+package net.continuumsecurity.v6.model;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by alcampos on 19/10/15.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Output {
+
+	private Map<String, String> ports;
+	private String has_attachment;
+	private String custom_description;
+	private String plugin_output;
+	private String severity;
+
+	public void setPlugin_output(String plugin_output) {
+		this.plugin_output = plugin_output;
+	}
+
+	public void setPorts(Map<String, String> ports) {
+		this.ports = ports;
+	}
+
+	public void setHas_attachment(String has_attachment) {
+		this.has_attachment = has_attachment;
+	}
+
+	public void setCustom_description(String custom_description) {
+		this.custom_description = custom_description;
+	}
+
+	public void setSeverity(String severity) {
+		this.severity = severity;
+	}
+
+	public String getCustom_description() {
+		return custom_description;
+	}
+
+	public String getHas_attachment() {
+		return has_attachment;
+	}
+
+	public String getPlugin_output() {
+		return plugin_output;
+	}
+
+	public Map<String, String> getPorts() {
+		return ports;
+	}
+
+	public String getSeverity() {
+		return severity;
+	}
+
+}
--- src/main/java/net/continuumsecurity/v6/model/PluginOutput.java
+++ src/main/java/net/continuumsecurity/v6/model/PluginOutput.java
@@ -1,5 +1,7 @@
 package net.continuumsecurity.v6.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
@@ -8,12 +10,21 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PluginOutput {
 	private InfoV6 info;
+	private List<Output> outputs;
 
 	public InfoV6 getInfo() {
 		return info;
 	}
-	
+
 	public void setInfo(InfoV6 info) {
 		this.info = info;
+	}
+
+	public List<Output> getOutputs() {
+		return outputs;
+	}
+
+	public void setOutputs(List<Output> outputs) {
+		this.outputs = outputs;
 	}
 }
